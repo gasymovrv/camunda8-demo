aedfa58c20cc785121c3a4308217e2e20fdfa59f
Ruslan Gasymov <RGasymov@luxoft.com>
2022-09-11 03:11:05 +0400

    Add parallel test

--- src/main/kotlin/com/example/camunda8demo/Workers.kt
+++ src/main/kotlin/com/example/camunda8demo/Workers.kt
@@ -3,50 +3,57 @@ package com.example.camunda8demo
 import io.camunda.zeebe.client.api.response.ActivatedJob
 import io.camunda.zeebe.client.api.worker.JobClient
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class Workers {
+    val log: Logger = LoggerFactory.getLogger(javaClass)
+
     @ZeebeWorker(type = "simple_task1", autoComplete = true)
     fun handleSimpleTask1(job: ActivatedJob) {
         println("execute simple_task 1...")
         job.print()
-        println("----- finish simple_task1 -----")
+        println("----- finish simple_task1 -----\n")
     }
 
     @ZeebeWorker(type = "simple_task2", autoComplete = true)
     fun handleSimpleTask2(job: ActivatedJob) {
         println("execute simple_task 2...")
         job.print()
-        println("----- finish simple_task2 -----")
+        println("----- finish simple_task2 -----\n")
     }
 
     @ZeebeWorker(type = "retryable_task")
-    fun handleRetryableTask(jobClient: JobClient, job: ActivatedJob) {
-        println("execute retryable_task...")
+    fun handleRetryableTask(jobClient: JobClient, job: ActivatedJob) = runBlocking {
+        log.info("=============== execute retryable_task, instance: ${job.processInstanceKey} ==================")
         if (job.retries > 1) { // emulate fails
             val remainAttempts = job.retries - 1
+            delay(2000)
             jobClient
                 .newFailCommand(job)
                 .retries(remainAttempts)
                 .errorMessage("everything is bad... we will see it in incident when the retries are over")
                 .send()
                 .whenComplete { _, _ ->
-                    println("failed attempt, remain attempts: $remainAttempts")
+                    log.info("instance '${job.processInstanceKey}' - failed attempt, remain attempts: $remainAttempts")
                 }
         } else {
             jobClient.newCompleteCommand(job)
                 .send()
                 .whenComplete { _, err -> callBack(job, err) }
         }
-        println("----- finish retryable_task -----")
+        log.info("=============== finish retryable_task, instance: ${job.processInstanceKey} ==================")
     }
 
     @ZeebeWorker(type = "long_task", autoComplete = true)
     fun handleLongTask(job: ActivatedJob) {
         println("execute long_task...")
         Thread.sleep(60000)
-        println("----- finish long_task -----")
+        println("----- finish long_task -----\n")
     }
 
     private fun ActivatedJob.print() {
